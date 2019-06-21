8aec949fc57911499c2fe5b396b74bf24de9ebc3
kDawg Jet$ki <kandarpa9@gmail.com>
2019-06-20 22:56:45 -0400

    Login secured from file

--- src/GetDefinition.java
+++ src/GetDefinition.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.codec.binary.Base64;
@@ -11,17 +12,20 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GetDefinition {
 
-    public static void main(String[] args)
-    {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\rudra\\OneDrive\\Desktop\\Selenium Drivers\\chromedriver.exe");
+    public static void main(String[] args) throws IOException {
+        File file = new File("loginCredentials.txt");
+        BufferedReader input = new BufferedReader(new FileReader(file));
+        String loginUser = input.readLine();
+        String loginPassword = input.readLine();
+        System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         String url = "https://portal.mypearson.com/portal";
         driver.get(url);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebElement email_phone = driver.findElement(By.xpath("/html[@class='ng-scope']/body[@id='appRoot']/div[@id='page-top']/div[@id='e2e-comms-wrapper']/div[@class='auth-layer-wrapper ng-scope']/div[@class='auth-layer ng-scope']/div[@class='pe-template__double']/div[@class='pe-template__double--main']/div[@class='auth-content container ng-scope']/div[@class='auth-form ng-scope']/main[@class='ng-scope'][3]/form[@id='mainForm']/div[@class='pe-form--group'][1]/input[@id='username']"));
-        email_phone.sendKeys("rdd33@njit.edu");
+        email_phone.sendKeys(loginUser);
         WebElement password = driver.findElement(By.xpath("/html[@class='ng-scope']/body[@id='appRoot']/div[@id='page-top']/div[@id='e2e-comms-wrapper']/div[@class='auth-layer-wrapper ng-scope']/div[@class='auth-layer ng-scope']/div[@class='pe-template__double']/div[@class='pe-template__double--main']/div[@class='auth-content container ng-scope']/div[@class='auth-form ng-scope']/main[@class='ng-scope'][3]/form[@id='mainForm']/div[@class='pe-form--group'][2]/input[@id='password']"));
-        password.sendKeys("");
+        password.sendKeys(loginPassword);
     }
 }
